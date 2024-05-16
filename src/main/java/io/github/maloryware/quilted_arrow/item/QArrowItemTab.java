6ebc464d2f029d4975ac050eaacd186e00ef36f6
muz-yka <maxholling@gmail.com>
2024-05-16 22:21:14 +0100

    Changed quilted_arrow string to identifier QuiltedArrow.ID Replaced WOVEN_LEATHER_STRIP and WOVEN_LEATHER_PATCH ItemGroup to QUILTEDARROWCOMBAT

--- src/main/java/io/github/maloryware/quilted_arrow/item/QArrowItemTab.java
+++ src/main/java/io/github/maloryware/quilted_arrow/item/QArrowItemTab.java
@@ -1,10 +1,11 @@
 package io.github.maloryware.quilted_arrow.item;
 
+import io.github.maloryware.quilted_arrow.QuiltedArrow;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import org.quiltmc.qsl.item.group.api.QuiltItemGroup;
 
 public class QArrowItemTab {
-	public static final ItemGroup QUILTEDARROWCOMBAT = QuiltItemGroup.create(new Identifier("quilted_arrow", "combat"));
+	public static final ItemGroup QUILTEDARROWCOMBAT = QuiltItemGroup.create(new Identifier(QuiltedArrow.ID, "combat"));
 
 }

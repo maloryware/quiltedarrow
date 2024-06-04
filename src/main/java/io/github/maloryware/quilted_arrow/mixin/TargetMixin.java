9d8e29a8098bc45f108f82ff1cd2972ba7dbbd13
maloryware <mal.larsson.reyes@gmail.com>
2024-06-04 14:11:06 +0100

    mhm

--- src/main/java/io/github/maloryware/quilted_arrow/mixin/TargetMixin.java
+++ src/main/java/io/github/maloryware/quilted_arrow/mixin/TargetMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
+//yea
 @Debug(export = true)
 @Mixin(LivingEntity.class)
 public abstract class TargetMixin {

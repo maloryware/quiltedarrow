4894abd474613b2a5e897803c84ed1a887380b7c
maloryware <mal.larsson.reyes@gmail.com>
2024-07-05 19:49:30 +0100

    Added Balm/Waystones; Added GameRules doImmediateRespawn mixin

---
+++ src/main/java/io/github/maloryware/quilted_arrow/mixin/QRespawnMixin.java
@@ -0,0 +1,31 @@
+package io.github.maloryware.quilted_arrow.mixin;
+
+
+import net.minecraft.world.GameRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+
+
+@Mixin(GameRules.class)
+public class QRespawnMixin {
+
+	@ModifyArg(
+		method = "<clinit>",
+		slice = @Slice(
+			from = @At(value = "CONSTANT", args = "stringValue=doImmediateRespawn")
+
+		),
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/GameRules$BooleanRule;create(ZLjava/util/function/BiConsumer;)Lnet/minecraft/world/GameRules$Type;",
+			ordinal = 0
+		)
+
+
+	)
+	private static boolean injected(boolean thisbool){
+		return true;
+	}
+
+}
+
